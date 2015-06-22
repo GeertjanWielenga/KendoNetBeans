98f2768aa68aeee53ee4ca76658ce6d3c911b2e6
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-22 11:17:49 +0200

    fixing offset

--- src/org/netbeans/modules/javascript2/kendo/KendoUICodeCompletion.java
+++ src/org/netbeans/modules/javascript2/kendo/KendoUICodeCompletion.java
@@ -32,6 +32,7 @@ import org.openide.modules.InstalledFileLocator;
 public class KendoUICodeCompletion implements CompletionProvider {
 
     private static final List<File> kendoPropertyFiles = new ArrayList<File>();
+
     private static synchronized List<File> getDataFiles() {
         File kendoUIFolder = InstalledFileLocator.getDefault().locate(
                 "docs/api/javascript/ui",
@@ -55,73 +56,71 @@ public class KendoUICodeCompletion implements CompletionProvider {
 
     @Override
     public List<CompletionProposal> complete(CodeCompletionContext ccContext, CompletionContext jsCompletionContext, String prefix) {
-        if (jsCompletionContext != CompletionContext.OBJECT_PROPERTY_NAME) {
-            return Collections.EMPTY_LIST;
-        }
-        // find the object that can be configured
-        TokenHierarchy<?> th = ccContext.getParserResult().getSnapshot().getTokenHierarchy();
-        if (th == null) {
-            return Collections.EMPTY_LIST;
-        }
-        int carretOffset = ccContext.getCaretOffset();
-        int eOffset = ccContext.getParserResult().getSnapshot().getEmbeddedOffset(carretOffset);
-        TokenSequence<? extends JsTokenId> ts = LexUtilities.getJsTokenSequence(th, eOffset);
-        if (ts == null) {
-            return Collections.EMPTY_LIST;
-        }
-
-        ts.move(eOffset);
-
-        if (!ts.moveNext() && !ts.movePrevious()) {
-            return Collections.EMPTY_LIST;
-        }
-
-        Token<? extends JsTokenId> token = null;
-        JsTokenId tokenId;
-        //find the begining of the object literal
-        int balance = 1;
-        while (ts.movePrevious() && balance > 0) {
-            token = ts.token();
-            tokenId = token.id();
-            if (tokenId == JsTokenId.BRACKET_RIGHT_CURLY) {
-                balance++;
-            } else if (tokenId == JsTokenId.BRACKET_LEFT_CURLY) {
-                balance--;
+        if (jsCompletionContext == CompletionContext.OBJECT_PROPERTY_NAME) {
+            // find the object that can be configured
+            TokenHierarchy<?> th = ccContext.getParserResult().getSnapshot().getTokenHierarchy();
+            if (th == null) {
+                return Collections.EMPTY_LIST;
             }
-        }
-        if (token == null || balance != 0) {
-            return Collections.EMPTY_LIST;
-        }
-
-        // now we should be at the beginning of the object literal. 
-        token = LexUtilities.findPreviousToken(ts, Arrays.asList(JsTokenId.IDENTIFIER));
-        tokenId = token.id();
-        StringBuilder sb = new StringBuilder(token.text());
-        while ((tokenId == JsTokenId.IDENTIFIER || tokenId == JsTokenId.OPERATOR_DOT) && ts.movePrevious()) {
-            token = ts.token();
-            tokenId = token.id();
-            if (tokenId == JsTokenId.OPERATOR_DOT) {
-                sb.insert(0, '.'); // NOI18N
-            } else if (tokenId == JsTokenId.IDENTIFIER) {
-                sb.insert(0, token.text());
+            int carretOffset = ccContext.getCaretOffset();
+            int eOffset = ccContext.getParserResult().getSnapshot().getEmbeddedOffset(carretOffset);
+            TokenSequence<? extends JsTokenId> ts = LexUtilities.getJsTokenSequence(th, eOffset);
+            if (ts == null) {
+                return Collections.EMPTY_LIST;
             }
-        }
 
-        String fqnWithDot = sb.toString();
-        String fqn = fqnWithDot.substring(1).replace(".ui.", "");
-        Map<String, Collection<KendoUIDataItem>> data = getData();
-        Collection<KendoUIDataItem> items = data.get(fqn);
-        int anchorOffset = eOffset - ccContext.getPrefix().length();
-        if (items != null) {
-            StatusDisplayer.getDefault().setStatusText(fqn + ": code completion shown.");
-            List<CompletionProposal> result = new ArrayList<CompletionProposal>();
-            for (KendoUIDataItem item : items) {
-                if (fqn.startsWith(prefix)) {
-//                if (item.getName().startsWith(prefix)) {
-                    result.add(KendoUICompletionItem.createKendoUIItem(item, anchorOffset));
+            ts.move(eOffset);
+
+            if (!ts.moveNext() && !ts.movePrevious()) {
+                return Collections.EMPTY_LIST;
+            }
+
+            Token<? extends JsTokenId> token = null;
+            JsTokenId tokenId;
+            //find the begining of the object literal
+            int balance = 1;
+            while (ts.movePrevious() && balance > 0) {
+                token = ts.token();
+                tokenId = token.id();
+                if (tokenId == JsTokenId.BRACKET_RIGHT_CURLY) {
+                    balance++;
+                } else if (tokenId == JsTokenId.BRACKET_LEFT_CURLY) {
+                    balance--;
                 }
             }
-            return result;
+            if (token == null || balance != 0) {
+                return Collections.EMPTY_LIST;
+            }
+
+            // now we should be at the beginning of the object literal. 
+            token = LexUtilities.findPreviousToken(ts, Arrays.asList(JsTokenId.IDENTIFIER));
+            tokenId = token.id();
+            StringBuilder sb = new StringBuilder(token.text());
+            while ((tokenId == JsTokenId.IDENTIFIER || tokenId == JsTokenId.OPERATOR_DOT) && ts.movePrevious()) {
+                token = ts.token();
+                tokenId = token.id();
+                if (tokenId == JsTokenId.OPERATOR_DOT) {
+                    sb.insert(0, '.'); // NOI18N
+                } else if (tokenId == JsTokenId.IDENTIFIER) {
+                    sb.insert(0, token.text());
+                }
+            }
+
+            String fqnWithDot = sb.toString();
+            String fqn = fqnWithDot.substring(1).replace(".ui.", "");
+            Map<String, Collection<KendoUIDataItem>> data = getData();
+            Collection<KendoUIDataItem> items = data.get(fqn);
+            int caretOffset = ccContext.getCaretOffset();
+            if (items != null) {
+                StatusDisplayer.getDefault().setStatusText(fqn + ": code completion shown.");
+                List<CompletionProposal> result = new ArrayList<CompletionProposal>();
+                for (KendoUIDataItem item : items) {
+                    if (fqn.startsWith(prefix)) {
+                        result.add(KendoUICompletionItem.createKendoUIItem(item, caretOffset));
+                    }
+                }
+                return result;
+            }
         }
         return Collections.EMPTY_LIST;
     }
